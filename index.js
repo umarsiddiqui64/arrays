e887bafe6442de730aa081b47fb27b502b617c63
Umar Siddiqui <umarsiddiqui64@gmail.com>
2024-12-04 13:04:48 +0530

    Solved 2D Array Problem

---
+++ index.js
@@ -0,0 +1,12 @@
+let N = 3; 
+let M = 2; 
+let arr = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+for (let i = 0; i < N; i++) {
+    let rowString = arr[i].join(" ");
+    console.log(rowString);
+}
